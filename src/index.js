3ecaceeb29e5ccbd27a07ac05030862200231ff1
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-10-23 14:17:27 +0200

    create update, redner, init functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log('hello world')
+let mainContainer = null
+
+const update = () => {
+
+    mainContainer.innerHTML = ''
+
+    const app = render()
+
+    mainContainer.appendChild(app)
+
+}
+
+const render = () => {
+
+    const container = document.createElement('div')
+
+    return container
+
+}
+const init = (containerSelector = '#root') => {
+
+    const container = document.querySelector(containerSelector)
+
+    if (!container) {
+        console.error('Type correct container')
+    }
+
+    mainContainer = container
+
+    const app = render()
+
+    container.appendChild(app)
+
+}
+init()
