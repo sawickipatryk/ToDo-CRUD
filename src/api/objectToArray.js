1c6b59872d34163c6bb3fabc5de9e70725afa8d3
Patryk Sawicki <sawicki.patryk129@gmail.com>
2023-10-23 14:54:28 +0200

    create objectToArray

---
+++ src/api/objectToArray.js
@@ -0,0 +1,23 @@
+export const objectToArray = (object, keyPropertyName = 'id') => {
+
+    return (
+        Object
+            .entries(object || {})
+            .map((entry) => {
+                const key = entry[0]
+                const value = entry[1]
+
+                if (typeof value === 'object') {
+                    value[keyPropertyName] = key
+                    return value
+                }
+
+                return {
+                    [keyPropertyName]: key,
+                    value: key
+                }
+            })
+    )
+
+}
+export default objectToArray
